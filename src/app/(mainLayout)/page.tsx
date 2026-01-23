c7d060d176cb452b9d7c6e73c9d71aa0272513e5
SajidSojib <sajidahmed2401@gmail.com>
2026-01-24 00:59:37 +0600

    session get in server component

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,8 +1,18 @@
 import { authClient } from '@/lib/auth-client';
+import { cookies } from 'next/headers';
 import React from 'react';
 
 const Home = async() => {
-    const session = await authClient.getSession();
+    // const session = await authClient.getSession();   // always return null in server component
+
+    const cookieStore = await cookies();
+    const res = await fetch("http://localhost:8000/api/auth/get-session", {
+        headers:{
+            cookie: cookieStore.toString()
+        },
+        cache: "no-store"
+    })
+    const session = await res.json();
     console.log("session from home", session);
     return (
         <div>
