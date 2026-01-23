20cb1c84169aaeb0e5d15fb9e0a1d93c5b646bf9
SajidSojib <sajidahmed2401@gmail.com>
2026-01-24 01:54:37 +0600

    t3 oss and zod added

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,29 +1,29 @@
+import { env } from "@/env";
 import { cookies } from "next/headers";
 
+const AUTH_URL = env.AUTH_URL;
+
 export const userService = {
-    getSession : async function () {
-        try {
-          // const session = await authClient.getSession();   // always return null in server component
+  getSession: async function () {
+    try {
+      // const session = await authClient.getSession();   // always return null in server component
 
-          const cookieStore = await cookies();
-          const res = await fetch(
-            "http://localhost:8000/api/auth/get-session",
-            {
-              headers: {
-                cookie: cookieStore.toString(),
-              },
-              cache: "no-store",
-            }
-          );
-          const session = await res.json();
+      const cookieStore = await cookies();
+      const res = await fetch(`${AUTH_URL}/get-session`, {
+        headers: {
+          cookie: cookieStore.toString(),
+        },
+        cache: "no-store",
+      });
+      const session = await res.json();
 
-          if(!session) {
-            return {data: null, error: {message: "Session not found"}}
-          }
-          return {data: session, error: null}
-        } catch (error) {
-            console.log(error);
-            return {data: null, error: {message: "Something went wrong"}}
-        }
+      if (!session) {
+        return { data: null, error: { message: "Session not found" } };
+      }
+      return { data: session, error: null };
+    } catch (error) {
+      console.log(error);
+      return { data: null, error: { message: "Something went wrong" } };
     }
-}
+  },
+};
