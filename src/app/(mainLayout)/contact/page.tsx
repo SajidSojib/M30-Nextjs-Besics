50a99f4c2847a48f698ea1427df04b8de7e9b365
SajidSojib <sajidahmed2401@gmail.com>
2026-01-27 01:21:04 +0600

    implemented tanstack form and zod validation

--- src/app/(mainLayout)/contact/page.tsx
+++ src/app/(mainLayout)/contact/page.tsx
@@ -1,16 +1,16 @@
-"use client"
+"use client";
 
 import { getAllBlogPosts } from "@/actions/blog.action";
 import { BlogPost } from "@/types/blog.type";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export default function ContactPage() {
   const [blogs, setBlogs] = useState([]);
   useEffect(() => {
-    ( async () => {
+    (async () => {
       const { data } = await getAllBlogPosts();
       setBlogs(data);
-    })()
+    })();
   }, []);
   return (
     <div>
@@ -19,5 +19,5 @@ export default function ContactPage() {
         <div key={blog?.id}>{blog?.title}</div>
       ))}
     </div>
-  )
+  );
 }
