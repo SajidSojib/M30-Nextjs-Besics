a19db303790ee8e6a34a4a670c84e4d03b7896c2
SajidSojib <sajidahmed2401@gmail.com>
2026-01-27 17:34:32 +0600

    image optimization done

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -30,11 +30,11 @@ const Home = async () => {
     <div className="max-w-7xl mx-auto px-4">
       <div className="mb-12 mt-8 h-[calc(100vh-80px)] flex flex-col justify-center">
         <div className="relative w-full h-96 mb-6">
-          <img
+          {/* <img
             src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?w=1920&q=100"
             alt="Hero"
             className="w-full h-full object-cover rounded-lg"
-          />
+          /> */}
 
           {/* <Image
             src={coffee}
@@ -42,13 +42,13 @@ const Home = async () => {
             alt="Hero"
             className="object-cover rounded-md"
           /> */}
-          {/* <Image
+          <Image
               src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?w=1920&q=100"
               fill
               priority
               alt="Hero"
               className="object-cover rounded-md"
-            /> */}
+            />
         </div>
         <h1 className={"text-5xl font-bold text-center mb-4"}>
           Welcome to Our Blog
